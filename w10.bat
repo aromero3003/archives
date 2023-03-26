931165036324938a6762e387ea5528b280984a56
Alan Romero <86857284+aromero3003@users.noreply.github.com>
2023-03-26 05:44:19 -0300

    fix: que no se vean los comandos

--- w10.bat
+++ w10.bat
@@ -1,5 +1,5 @@
-echo Acepta los tres carteles siguientes men
 @echo off
+echo Acepta los tres carteles siguientes men
 slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
 slmgr /skms kms.digiboy.ir
 slmgr /ato
